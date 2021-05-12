a2aa5bd473d17a24a31395db053186b4982543df
Talia2019 <chung1997jy@gmail.com>
2021-05-12 17:23:58 +0900

    detail page and sort

--- pages/Detail.js
+++ pages/Detail.js
@@ -2,17 +2,28 @@ import React from 'react';
 import Container from '../components/Container';
 import Contents from '../components/Contents';
 import styled from 'styled-components';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Text = styled.Text`
     font-size: 20px;
     line-height: 20px;
 `;
 
-function Detail({navigation}){
+function Detail({navigation, route}){
+    const [text, setText] = React.useState('');
+    React.useEffect(()=>{
+        navigation.setOptions({title: route.params.date});
+        AsyncStorage.getItem('list')
+            .then(data=>{
+                const list = JSON.parse(data);
+                const diary = list.find(element => element.date === route.params.date);
+                setText(diary.text);
+            })
+    }, []);
     return(
         <Container>
             <Contents>
-                <Text>{'hello'}</Text>
+                <Text>{text}</Text>
             </Contents>
         </Container>
     )
